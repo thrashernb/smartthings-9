a3e777491d06354f273beebe6f6d49bae7bd6426
Kenny <r3dey3@r3dey3.com>
2015-12-27 23:44:56 -0500

    Actual LED control

---
+++ api/fakestrip.py
@@ -0,0 +1,11 @@
+class LedStrip(object):
+	def __init__(self, leds=32, dev="/dev/spidev0.1"):
+		pass
+	def fill(self, r, g, b, start=0, end=0):
+		pass
+	def set(self, pixel, r, g, b):
+		pass
+	def update(self):
+		pass
+	
+
--- api/main.py
+++ api/main.py
@@ -1,38 +1,88 @@
 from flask import Flask
 from flask import request
 from flask_json import FlaskJSON, JsonError, json_response, as_json
+import ledstrip
+#import fakestrip as ledstrip
+import colorsys
+
+NUM_LEDS=120
 
 app = Flask(__name__)
 FlaskJSON(app)
 
+strip = ledstrip.LedStrip(NUM_LEDS)
 
 state = {
     "switch": "off",
     "level": 100,
-    "color": "#ffffff",
-    "switch2": "off",
     "level2": 100,
-    "color2": "#ffffff"
+
+    "color_raw": {"hue": 0.0, "saturation": 0.0},
+    "color2_raw": {"hue": 0.0, "saturation": 0.0},
+    "mode": "single_color"
 }
 
+
+def update(new_data = {}):
+    global state
+    state.update(new_data)
+    print "\nState = %r" % (state)
+    try:
+        if state["switch"] == "off":
+            strip.fill(0,0,0)
+        elif state["mode"] == "rainbow":
+            for i in range(0, NUM_LEDS):
+                rgb = colorsys.hsv_to_rgb(i/float(NUM_LEDS), state["level2"]/100.0, state["level"]/100.0)
+                r, g, b = [ min(255, int(256*v)) for v in rgb ]
+                strip.set(i, r,g,b)
+            strip.update()
+        elif state["mode"] == "single_color":
+            rgb = colorsys.hsv_to_rgb(state["color_raw"]["hue"]/100.0, state["color_raw"]["saturation"]/100.0, state["level"]/100.0)
+            r, g, b = [ min(255, int(256*i)) for i in rgb ]
+            strip.fill(r,g,b)
+        elif state["mode"] == "dual_color":
+            rgb = colorsys.hsv_to_rgb(state["color_raw"]["hue"]/100.0, state["color_raw"]["saturation"]/100.0, state["level"]/100.0)
+            r, g, b = [ min(255, int(256*i)) for i in rgb ]
+            strip.fill(r,g,b, 0, NUM_LEDS/2)
+
+            rgb = colorsys.hsv_to_rgb(state["color2_raw"]["hue"]/100.0, state["color2_raw"]["saturation"]/100.0, state["level2"]/100.0)
+            r2, g2, b2 = [ min(255, int(256*i)) for i in rgb ]
+            strip.fill(r2,g2,b2, NUM_LEDS/2)
+
+    except Exception as e:
+        print "ERROR: %r" % (e)
+
+MODES = ["single_color", "dual_color", "rainbow"]
 @app.route("/status")
 @as_json
 def get_status():
     global state
-    return state
+    ret = {k:v for k,v in state.iteritems() if not k.endswith("raw")}
+    return ret
 
 @app.route("/control", methods=["POST"])
 @as_json
 def control():
     global state
     data = request.get_json(force=True)
-    print repr(data)
-    print data
-    state.update(data)
-    print state
-    if state["switch"] == "off":
-        state["switch2"] = "off"
-    return state
+    update(data)
+    ret = {k:v for k,v in data.iteritems() if not k.endswith("raw")}
+    print "Returning %r" % (ret)
+    return ret
+
+@app.route("/next_mode", methods=["POST"])
+@as_json
+def next_mode():
+    global state
+    idx = MODES.index(state["mode"])
+    idx = (idx+1) % len(MODES)
+    state["mode"] = MODES[idx]
+    update()
+    ret= {"mode": state["mode"]}
+    print "Returning %r" % (ret)
+    return ret
 
 if __name__ == "__main__":
+    update()
     app.run(host="0.0.0.0")
+
