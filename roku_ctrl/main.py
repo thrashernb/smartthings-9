50946a83cd5b268a16d8cc90207a20ca7144678b
Kenny <r3dey3@r3dey3.com>
2016-06-17 22:32:16 -0500

    RokuControl work on python bridge

--- roku_ctrl/main.py
+++ roku_ctrl/main.py
@@ -1,9 +1,14 @@
+#!/usr/bin/env python
+
 from flask import Flask
 from flask import request
 from flask_json import FlaskJSON, JsonError, json_response, as_json
 import sys
 import threading
+import requests
+import time
 
+from roku import Roku
 
 app = Flask(__name__)
 FlaskJSON(app)
@@ -21,7 +26,7 @@ def control():
     global state
     data = request.get_json(force=True)
     update(data)
-    ret = {k:v for k,v in data.iteritems() if not k.startswith("_")}
+    ret = {k:v for k,v in state.iteritems() if not k.startswith("_")}
     print "Returning %r" % (ret)
     return ret
 
@@ -38,6 +43,7 @@ def load():
             new_state = json.load(f)
         print "Loaded", new_state
         state.update(**new_state)
+        state["switch"] = "on"
     except:
         print "Exception"
         pass
@@ -52,14 +58,75 @@ def do_update(state):
 
 def update(new_data = {}):
     global state
-    state.update(new_data)
     print "\nState = %r" % (state)
     try:
-        do_update(state)
+        do_update(new_data)
     except Exception as e:
         print "ERROR: %r" % (e)
     save()
     return state
+
+
+def do_update(new_data):
+    global roku_control
+    print new_data
+    try:
+        if new_data["switch"] == "off":
+            roku_control.power()
+            state.update(new_data)
+    except KeyError:
+        pass
+    print 'State = %r, new = %r' % (state, new_data)
+
+import threading
+class RokuControl(object):
+    def __init__(self):
+        self.device = None
+        self.thread = threading.Thread(target=self._monitor)
+        self.thread.daemon = True
+        self.thread.start()
+
+
+    def power(self):
+        try:
+            self.device.power()
+        except AttributeError:
+            pass
+    
+    @property
+    def on(self):
+        global state
+        return state["switch"] == "on"
+
+    @on.setter
+    def on(self, value):
+        global state
+        state["switch"] = ["off", "on"][value]
+        print "RETRIEVED STATE = %r" %(state)
+
+    def _monitor(self):
+        curState = ""
+        while True:
+            delay = 30
+            if not self.device:
+                devices = Roku.discover(timeout=10)
+                try:
+                    self.device = devices[0]
+                except IndexError:
+                    self.on = False
+
+            if self.device:
+                try:
+                    info = self.device.device_info
+                except requests.exceptions.ConnectionError:
+                    self.device = None
+                    self.on = False
+                    continue
+                self.on = info.power_mode == 'PowerOn'
+                if not self.on:
+                    delay = 90
+            #print time.time(), self.on, delay
+            time.sleep(delay)
 
 def main():
     load()
@@ -68,7 +135,8 @@ def main():
     sys.exit(0)
 
 if __name__ == "__main__":
-    main(strip)
+    roku_control = RokuControl()
+    main()
 
 
 
